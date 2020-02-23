f00f0b3d4bf477c65462805a419214b6829925ab
7natenine <46154700+7natenine@users.noreply.github.com>
2020-02-22 17:26:35 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,15 +20,15 @@ app.get('/', (req, res) => {
   })
 
 app.use(function errorHandler(error, req, res, next) {
-+   let response
-+   if (process.env.NODE_ENV === 'production') {
-+     response = { error: { message: 'server error' } }
-+   } else {
-+     console.error(error)
-+     response = { message: error.message, error }
-+   }
-+   res.status(500).json(response)
-+ })
+   let response
+   if (process.env.NODE_ENV === 'production') {
+     response = { error: { message: 'server error' } }
+   } else {
+     console.error(error)
+     response = { message: error.message, error }
+   }
+   res.status(500).json(response)
+ })
 
 
 module.exports = app 
